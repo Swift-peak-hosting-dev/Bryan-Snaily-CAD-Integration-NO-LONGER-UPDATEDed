0a16d07ce9cee009948467b5ef9f2b01031566f4
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2024-01-28 11:37:00 +0000

    Update config.lua

--- bryan_snaily/config.lua
+++ bryan_snaily/config.lua
@@ -6,6 +6,6 @@ Config.Defaults = {
 
 Config.API = {
     URL = 'https://api-test.swiftpeakhosting.co.uk/v1/',
-    TOKEN = 'sng_4TiV5Zuszyn9iDRQhnOpCXvrqPbvP9-VTF4SEl2uB5zYqXm1OkHIlD3R',
+    TOKEN = 'cad-token ',
     TOKEN_HEADER_NAME = 'snaily-cad-api-token',
 }
